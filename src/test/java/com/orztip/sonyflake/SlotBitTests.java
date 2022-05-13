5c0ae78484628c2231bcc370f364b8814a24b4be
horseluke <horseluke@126.com>
2022-05-13 16:31:36 +0800

    增加：SlotBitTests测试

--- src/test/java/com/orztip/sonyflake/SlotBitTests.java
+++ src/test/java/com/orztip/sonyflake/SlotBitTests.java
@@ -17,4 +17,25 @@ public class SlotBitTests {
 		
 	}
 	
+	
+	@Test
+	void testBitCalc2() {
+		
+		int mapIndexTotal = 255;
+		
+		assertEquals(0, 0 & mapIndexTotal);
+		assertEquals(1, 1 & mapIndexTotal);
+		assertEquals(255, 255 & mapIndexTotal);
+		assertEquals(0, 256 & mapIndexTotal);
+		assertEquals(1, 257 & mapIndexTotal);
+
+		long test1 = (2147483647L << 4) + 0xF;
+		int val = (int) (test1 & mapIndexTotal);
+		
+		assertEquals(255, val);
+
+		
+	}
+	
+	
 }
